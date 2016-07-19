3470a26d2d26db2215fbb6f0ceaee9dd4f40b3ee
Jane <ztang1253@gmail.com>
2016-07-19 15:07:31 -0400

    Integration Testing / Top-down

---
+++ ZTPROG2070Assign3/ZTPROG2070Assign3/ProjectC/Program.cs
@@ -0,0 +1,94 @@
+﻿/*  Program.cs
+ *  Assignment 3
+ * 
+ *  Revision History
+ *      Zhenzhen Tang, 2016.03.16: Created
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZTPROG2070Assign3;
+
+namespace ProjectC
+{
+    /// <summary>
+    /// Test class
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "";
+            double distance = 0;
+            string originalUnit = "";
+            string wishUnit = "";
+            string resultDistance = "";
+
+            while (true)
+            {
+                Console.WriteLine("------------- Please enter a valid distance (number) -------------");
+                inputString = Console.ReadLine();
+                try
+                {
+                    distance = Convert.ToDouble(inputString);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+                Console.WriteLine("");
+
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine("------------- Please enter the original unit -------------");
+                        Console.WriteLine("( i/I/inches/Inches/f/F/feet/Feet/y/Y/yards/Yards/m/M/miles/Miles )");
+                        originalUnit = Console.ReadLine();
+                        Console.WriteLine("");
+                        resultDistance = UnitConversion.Convert(distance, originalUnit, "i").ToString();
+                        break;
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        continue;
+                    }
+                }
+
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine("------------- Please enter the unit you wish to convert to -------------");
+                        Console.WriteLine("( i/I/inches/Inches/f/F/feet/Feet/y/Y/yards/Yards/m/M/miles/Miles )");
+                        wishUnit = Console.ReadLine();
+                        Console.WriteLine("");
+                        resultDistance = UnitConversion.Convert(distance, originalUnit, wishUnit).ToString();
+                        break;
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        continue;
+                    }
+                }
+
+                resultDistance = UnitConversion.Convert(distance, originalUnit, wishUnit).ToString();
+                Console.WriteLine(distance + " " + originalUnit + " = " + resultDistance + " " + wishUnit);
+                Console.WriteLine("");
+
+                Console.WriteLine("------------- Do you want to exit ? -------------");
+                Console.WriteLine("------------- Press y to exit, any other key to continue -------------");
+                inputString = Console.ReadLine().ToLower();
+                if (inputString == "y")
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
---
+++ ZTPROG2070Assign3/ZTPROG2070Assign3/ProjectC/UnitConversion.cs
@@ -0,0 +1,126 @@
+﻿/*  UnitConversion.cs
+ *  Assignment 3
+ * 
+ *  Revision History
+ *      Zhenzhen Tang, 2016.03.16: Created
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTPROG2070Assign3
+{
+    /// <summary>
+    /// Class to convert a length unit to another length unit
+    /// </summary>
+    public static class UnitConversion
+    {
+        public static double Convert(double value, string convertFrom, string convertTo)
+        {
+            double result = 0.0;
+            string modConvertFrom = ModifyInput(convertFrom);
+            string modConvertTo = ModifyInput(convertTo);
+
+            result = value * GetMultiplier(modConvertFrom, modConvertTo);
+            return result;
+        }
+
+        public static string ModifyInput(string input)
+        {
+            string modInput = "";
+
+            if (input == "i" || input == "I" || input == "inches" || input == "Inches")
+            {
+                modInput = "inches";
+            }
+            else if (input == "f" || input == "F" || input == "feet" || input == "Feet")
+            {
+                modInput = "feet";
+            }
+            else if (input == "y" || input == "Y" || input == "yards" || input == "Yards")
+            {
+                modInput = "yards";
+            }
+            else if (input == "m" || input == "M" || input == "miles" || input == "Miles")
+            {
+                modInput = "miles";
+            }
+            else
+            {
+                throw new ArgumentException("Incorrect Conversion Unit");
+            }
+
+            return modInput;
+        }
+
+        public static double GetMultiplier(string convertFrom, string convertTo)
+        {
+            double multiplier = 1.0;
+
+            switch (convertFrom.ToLower())
+            {
+                case "inches":
+                    if (convertTo == "feet")
+                    {
+                        multiplier = 1.0 / 12.0;
+                    }
+                    else if (convertTo == "yards")
+                    {
+                        multiplier = 1.0 / 12.0 / 3.0;
+                    }
+                    else if (convertTo == "miles")
+                    {
+                        multiplier = 1.0 / 12.0 / 3.0 / 1760.0;
+                    }
+                    break;
+                case "feet":
+                    if (convertTo == "inches")
+                    {
+                        multiplier = 12.0;
+                    }
+                    else if (convertTo == "yards")
+                    {
+                        multiplier = 1.0 / 3.0;
+                    }
+                    else if (convertTo == "miles")
+                    {
+                        multiplier = 1.0 / 3.0 / 1760.0;
+                    }
+                    break;
+                case "yards":
+                    if (convertTo == "inches")
+                    {
+                        multiplier = 3.0 * 12.0;
+                    }
+                    else if (convertTo == "feet")
+                    {
+                        multiplier = 3.0;
+                    }
+                    else if (convertTo == "miles")
+                    {
+                        multiplier = 1.0 / 1760.0;
+                    }
+                    break;
+                case "miles":
+                    if (convertTo == "inches")
+                    {
+                        multiplier = 12.0 * 3.0 * 1760.0;
+                    }
+                    else if (convertTo == "yards")
+                    {
+                        multiplier = 1760.0;
+                    }
+                    else if (convertTo == "feet")
+                    {
+                        multiplier = 3.0 * 1760.0;
+                    }
+                    break;
+            }
+
+            return multiplier;
+        }
+    }
+}
